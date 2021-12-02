ee1176c19c3c559ed4abcbe9de65204251efecbf
edemuner <eduardood15@hotmail.com>
2021-12-02 20:54:35 -0300

    aula encerrada

--- pecas.js
+++ pecas.js
@@ -24,3 +24,18 @@ if (nomePeca.length > 3){
 else{
     console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
 }
+
+switch (nomePeca.length){
+    case 0:
+        console.log("O nome da peça não pode ser vazio")
+        break;
+    case 1:
+    case 2:
+    case 3:
+        console.log("O nome deve ter mais de 3 caracteres, digite um nome adequado")
+        break;
+    default:
+        console.log("Nome da peça está adequado")
+        break;  
+
+}
